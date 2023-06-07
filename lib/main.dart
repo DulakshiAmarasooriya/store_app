711d4ad0ddc0ddc98eeb737d9ec6fa0973b51c06
DulakshiAmarasooriya <dulakshidilhani8@gmail.com>
2023-06-07 13:51:22 +0530

    The Application Theme

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:api_store_app/consts/global_colors.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Store App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: lightScaffoldColor,
+        primaryColor: lightCardColor,
+        backgroundColor: lightBackgroundColor,
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(
+            color: lightIconColor,
+          ),
+          backgroundColor: lightScaffoldColor,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: lightTextColor,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+          elevation: 0,
+        ),
+        iconTheme: IconThemeData(
+          color: lightIconColor,
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.black,
+          selectionColor: Colors.blue,
+        ),
+        cardColor: lightCardColor	,
+        brightness: Brightness.light,
+        colorScheme: ThemeData().colorScheme.copyWith(
+          secondary: lightIconColor,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: const HomeScreen(),
+      
+    );
+  }
+}
+
