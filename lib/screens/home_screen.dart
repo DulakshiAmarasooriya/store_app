26f2f36e7a0d5b992311bf931415545b136f6eda
DulakshiAmarasooriya <dulakshidilhani8@gmail.com>
2023-06-08 17:23:23 +0530

    The Products Grid View

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,30 +1,29 @@
-import 'package:api_store_app/consts/global_colors.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
-
+import '../consts/global_colors.dart';
 import '../widgets/appbar_icons.dart';
+import '../widgets/feeds_widget.dart';
 import '../widgets/sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late TextEditingController _texteditingcontroller;
-
+  late TextEditingController _textEditingController;
   @override
   void initState() {
-    _texteditingcontroller = TextEditingController();
+    _textEditingController = TextEditingController();
     super.initState();
   }
 
   @override
   void dispose() {
-    _texteditingcontroller.dispose();
+    _textEditingController.dispose();
     super.dispose();
   }
 
@@ -37,17 +36,19 @@ class _HomeScreenState extends State<HomeScreen> {
       },
       child: Scaffold(
           appBar: AppBar(
-            elevation: 4.0,
+            // elevation: 4,
             title: const Text('Home'),
-            leading: Flexible(
-              child: AppBarIcons(
-                function: () {},
-                icon: IconlyBold.category,
-              ),
+            leading: AppBarIcons(
+              function: () {
+                
+              },
+              icon: IconlyBold.category,
             ),
             actions: [
               AppBarIcons(
-                function: () {},
+                function: () {
+                 
+                },
                 icon: IconlyBold.user_3,
               ),
             ],
@@ -56,49 +57,90 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
+                const SizedBox(
+                  height: 18,
+                ),
                 TextField(
-                  controller: _texteditingcontroller,
+                  controller: _textEditingController,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
-                    hintText: "Search",
-                    filled: true,
-                    fillColor: Theme.of(context).cardColor,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                        color: Theme.of(context).cardColor,
+                      hintText: "Search",
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).cardColor,
+                        ),
                       ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                        width: 1,
-                        color: Theme.of(context).colorScheme.secondary,
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
                       ),
-                    ),
-                    suffix: Icon(
-                      IconlyLight.search,
-                      color: lightIconColor,
-                    ),
-                  ),
+                      suffixIcon: Icon(
+                        IconlyLight.search,
+                        color: lightIconColor,
+                      )),
                 ),
-                SizedBox(
-                  height: size.height * 0.25,
-                  child: Swiper(
-                    itemCount: 3,
-                    itemBuilder: (ctx, index) {
-                      return const SaleWidget();
-                    },
-                    pagination: const SwiperPagination(
-                      alignment: Alignment.bottomCenter,
-                      builder: DotSwiperPaginationBuilder(
-                        color: Colors.white,
-                        activeColor: Colors.red,
-                      ),
-                    ),
-                    // control: const SwiperControl(),
-                  ),
+                const SizedBox(
+                  height: 18,
                 ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) {
+                            return const SaleWidget();
+                          },
+                          autoplay: true,
+                          pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: Colors.red)),
+                          // control: const SwiperControl(),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                              "Latest Products",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const Spacer(),
+                            AppBarIcons(
+                                function: () {},
+                                icon: IconlyBold.arrow_right_2),
+                          ],
+                        ),
+                      ),
+                      GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: 3,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 0.0,
+                                  mainAxisSpacing: 0.0,
+                                  childAspectRatio: 0.7),
+                          itemBuilder: (ctx, index) {
+                            return const FeedsWidget();
+                          })
+                    ]),
+                  ),
+                )
               ],
             ),
           )),
